e74408fffc0331c0114af1ff7a04c47583eedccd
Karthik Dulam <63772598+DKarthik2222@users.noreply.github.com>
2020-12-15 18:14:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 $(document).ready(() => {
-  var url =
-    "http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D";
+  var url = "https://5fc0748dfd14be0016749cfe.mockapi.io/karthik/auth";
   var tableData;
   const dataPromise = new Promise((resolve, reject) => {
     $.get(url, (data) => {
